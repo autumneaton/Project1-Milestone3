0d12c2daf277a85853203ab00145f3373d35059f
autumneaton <119529242+autumneaton@users.noreply.github.com>
2024-11-06 15:00:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 let express = require("express");
-let app = new express();                                                                     
-
+let app = new express();
 // set up database connection
 const knex = require("knex")({
- client: "mysql",
- connection: {
-  host:"concert-db-instance-1.c61dq6ysma4i.us-east-2.rds.amazonaws.com",
-  user: "admin",
-  password: "Password1",
-  database:"paradise-concerts",
-  port: 3306,
- },
+client: "mysql",
+connection: {
+host: "",
+user: "admin",
+password: "password",
+database: "dinosaurs",
+port: 3306
+},
 });
 
-app.get("/",(req,res) => {
- knex
- .select()
- .from("venues")
- .then((result) => {
-  console.log(result);
-  res.send(result);
- }); 
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    knex.select().from("dinosaur")
+    .then((result) => {
+    console.log(result);
+    //Render the index.ejs template and pass the result
+    res.render("index", {dinosaurs: result});
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error fetching data");
+    });
 });
-app.listen(3000);
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+app.use('/views', express.static('views'));
